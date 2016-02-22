f4f140963801be4e75b720b638fb89c4d329b42d
paulshorey <ps@artspaces.net>
2016-02-21 23:59:43 -0500

    .sh

---
+++ _boot/app.sh
@@ -0,0 +1,4 @@
+# while true; do
+# 	casperjs bot.js
+# 	sleep 60
+# done
---
+++ _boot/files.sh
@@ -0,0 +1,3 @@
+iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8000
+rm -rf console
+# http-server console -p 8000 -s -c 30 #-d false
