8f1ef4164432f0114876230be4418a83ff9e140c
paulshorey <ps@artspaces.net>
2016-02-22 00:24:59 -0500

    /www/...

--- _boot/files.sh
+++ _boot/files.sh
@@ -1,3 +1,3 @@
 iptables -A PREROUTING -t nat -i eth0 -p tcp --dport 80 -j REDIRECT --to-port 8000
-rm -rf public/console/logs
-http-server public -p 8000 -s -c 30 #-d false
+rm -rf /www/public/console/logs
+http-server /www/public/console -p 8000 -s -c 30 #-d false
