358b1759c028e063c89f44b2a284c6fa8cbb1319
paulshorey <ps@artspaces.net>
2016-02-27 17:09:32 -0500

    no autorun

--- _boot/app.sh
+++ _boot/app.sh
@@ -1,7 +1,7 @@
-cd /www/bot-nyc
-i=0;
-while true; do
-	i=$[$i+1]
-	casperjs bot.js --iteration=$i
-	sleep 60
-done
+# cd /www/bot-nyc
+# i=0;
+# while true; do
+# 	i=$[$i+1]
+# 	casperjs bot.js --iteration=$i
+# 	sleep 60
+# done
