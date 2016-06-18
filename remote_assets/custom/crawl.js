f2cb84f8c8c2755069a8018091fc1b14aed1d0bd
paul (local) <ps@artspaces.net>
2016-06-18 10:30:51 -0400

    if elem.length

--- remote_assets/custom/crawl.js
+++ remote_assets/custom/crawl.js
@@ -73,11 +73,13 @@ window.casbot.crawl = function(each) {
 				} catch(e) {
 					var elem = $(this).find(each.site.selectors.item_dates[c]);
 				}
-				for (var e in elem) {
-					// use
-					var timestamp = casbot.stackTime(stack, elem[0].innerText);
-					if (!timestamp) {
-						console.log('### Manual date selector did not work');
+				if (elem.length) {
+					for (var e in elem) {
+						// use
+						var timestamp = casbot.stackTime(stack, elem[0].innerText);
+						if (!timestamp) {
+							console.log('### Manual date selector did not work');
+						}
 					}
 				}
 				stack.index++;
